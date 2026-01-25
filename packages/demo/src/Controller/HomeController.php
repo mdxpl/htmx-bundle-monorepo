6871f66512434885ef0ccbcecdb3df78e2745456
mdxpl <mateusz@batmaid.com>
2026-01-25 20:15:11 +0100

    Cache demo homepage

--- packages/demo/src/Controller/HomeController.php
+++ packages/demo/src/Controller/HomeController.php
@@ -6,9 +6,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/', name: 'app_home')]
+#[Cache(maxage: 86400, public: true)]
 final class HomeController extends AbstractController
 {
     public function __invoke(): Response
